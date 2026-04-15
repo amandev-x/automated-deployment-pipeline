c85bc54be2c797cb944fba23af6e29574fd218dd
initaman <amandabral2104@gmail.com>
2026-04-15 12:49:17 +0530

    add access to jenkins-instance from any ip temporary

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -25,7 +25,7 @@ resource "aws_vpc_security_group_ingress_rule" "jenkins_web_ui_ingress" {
   ip_protocol       = "tcp"
   from_port         = 8080
   to_port           = 8080
-  cidr_ipv4         = var.own_ip
+  cidr_ipv4         = "0.0.0.0/0"
 
   tags = merge(local.common, {
     Name = "${var.project_name}-jenkins-web-ui-ingress"
@@ -42,16 +42,16 @@ resource "aws_vpc_security_group_egress_rule" "egress" {
   })
 }
 
-# Kinds security group
-resource "aws_security_group" "kinds_sg" {
-  name        = "${var.project_name}-kinds-sg"
-  description = "Security group for Kinds EC2 instance"
+# K3s security group
+resource "aws_security_group" "k3s_sg" {
+  name        = "${var.project_name}-k3s-sg"
+  description = "Security group for K3s EC2 instance"
   vpc_id      = aws_vpc.main_vpc.id
 }
 
-# SSH from own IP to kinds instance
-resource "aws_vpc_security_group_ingress_rule" "kind_ssh_ingress" {
-  security_group_id = aws_security_group.kinds_sg.id
+# SSH from own IP to k3s instance
+resource "aws_vpc_security_group_ingress_rule" "k3s_ssh_ingress" {
+  security_group_id = aws_security_group.k3s_sg.id
   ip_protocol       = "tcp"
   from_port         = 22
   to_port           = 22
@@ -59,38 +59,38 @@ resource "aws_vpc_security_group_ingress_rule" "kind_ssh_ingress" {
 }
 
 # Kubernetes API — Jenkins needs this to run kubectl/helm commands
-resource "aws_vpc_security_group_ingress_rule" "jenkins_to_kind_ingress" {
-  security_group_id            = aws_security_group.kinds_sg.id
+resource "aws_vpc_security_group_ingress_rule" "jenkins_to_k3s_ingress" {
+  security_group_id            = aws_security_group.k3s_sg.id
   ip_protocol                  = "tcp"
   from_port                    = 6443
   to_port                      = 6443
   referenced_security_group_id = aws_security_group.jenkins_sg.id
 
   tags = merge(local.common, {
-    Name = "${var.project_name}-jenkins-to-kinds-ingress"
+    Name = "${var.project_name}-jenkins-to-k3s-ingress"
   })
 }
 
 # NodePort range — to access deployed apps during verification
 resource "aws_vpc_security_group_ingress_rule" "nodeport_ingress" {
-  security_group_id            = aws_security_group.kinds_sg.id
+  security_group_id            = aws_security_group.k3s_sg.id
   ip_protocol                  = "tcp"
   from_port                    = 30000
   to_port                      = 32767
   referenced_security_group_id = aws_security_group.jenkins_sg.id
 
   tags = merge(local.common, {
-    Name = "${var.project_name}-kinds-nodeport-ingress"
+    Name = "${var.project_name}-k3s-nodeport-ingress"
   })
 }
 
 # All outbound — needed to pull images from ECR
-resource "aws_vpc_security_group_egress_rule" "kind_egress" {
-  security_group_id = aws_security_group.kinds_sg.id
+resource "aws_vpc_security_group_egress_rule" "k3s_egress" {
+  security_group_id = aws_security_group.k3s_sg.id
   ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
 
   tags = merge(local.common, {
-    Name = "${var.project_name}-kinds-sg-egress"
+    Name = "${var.project_name}-k3s-sg-egress"
   })
 }
